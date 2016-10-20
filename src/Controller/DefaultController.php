8b355e9b53782b658089fc1f26d804e83931472f
Álvaro <toolsprods@gmail.com>
2016-10-20 15:34:14 +0200

    Initial commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\latch\Controller\DefaultController.
+ */
+namespace Drupal\latch\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\latch\LatchApp as Latch;
+
+/**
+ * Default controller for the latch module.
+ */
+class DefaultController extends ControllerBase {
+
+	public function getLatchId($uid) {
+	    $query = db_query("SELECT * FROM {latch} WHERE uid=:uid", array(':uid' => $uid));
+	    $result = $query->fetchObject();
+	    return ($result) ? $result->latch_account : NULL;
+	}
+
+	public function pairAccount($token) {
+		$config = \Drupal::config('latch.settings');
+		$appid = $config->get('latch_appid');
+		$secret = $config->get('latch_secret');
+		$uid = \Drupal::currentUser()->id();
+
+		$api = new Latch($appid, $secret);
+		$pairResponse = $api->pair($token);
+		$responseData = $pairResponse->getData();
+
+		if (!empty($responseData)) {
+	        $accountId = $responseData->{"accountId"};
+	    }
+	    if (!empty($accountId)) {
+	        //OK PAIRING
+	        db_insert('latch')->fields(array(
+	            'uid' => $uid,
+	            'latch_account' => $accountId
+	        ))->execute();
+	        drupal_set_message('Pairing success');
+	    } else {
+	        //NOT PAIRING
+	        drupal_set_message('Pairing token not valid', 'error');
+	    }
+	}
+
+	public function unpairAccount($latch_account) {
+		$config = \Drupal::config('latch.settings');
+		$appid = $config->get('latch_appid');
+		$secret = $config->get('latch_secret');
+		$uid = \Drupal::currentUser()->id();
+
+		$api = new Latch($appid, $secret);
+		$pairResponse = $api->unpair($latch_account);
+
+		db_delete('latch')->condition('uid', $uid)->execute();
+		drupal_set_message('Unpairing success');
+	}
+
+	public function getLatchStatus($latch_account) {
+	    $appid = \Drupal::config('latch.settings')->get('latch_appid');
+	    $secret = \Drupal::config('latch.settings')->get('latch_secret');
+	    if (!empty($appid) && !empty($secret)) {
+	        $api = new Latch($appid, $secret);
+	        return $api->status($latch_account);
+	    } else {
+	        return new LatchResponse("");
+	    }
+	}
+
+	public function processStatusResponse($statusResponse, $account) {
+	    $appid = \Drupal::config('latch.settings')->get('latch_appid');
+	    $responseData = $statusResponse->getData();
+	    $responseError = $statusResponse->getError();
+
+	    // If something goes wrong, disable Latch temporary or permanently to prevent blocking the user
+	    if (empty($statusResponse) || (empty($responseData) && empty($responseError))) {
+	        // Login success to prevent DoS to the user         
+	    } else {
+	        if (!empty($responseError) && $responseError->getCode() == 201) {
+	            // If the account is externally unpaired, apply the changes in database            
+	            db_delete('latch')->condition('uid', $account->id());
+	        }
+	        if (!empty($responseData) && DefaultController::isStatusOn($responseData, $appid)) {
+	            // LOGIN OK + STATUS = on
+	            if (DefaultController::isSecondFactorEnabled($responseData, $appid)) {
+	                $otp = $responseData->{"operations"}->{$appid}->{"two_factor"}->{"token"};
+	                DefaultController::storeSecondFactor($otp, $account->id());
+	                session_destroy(); // The user cannot be authenticated yet
+	                echo DefaultController::buildHTML(\Drupal::formBuilder()->getForm('Drupal\latch\Form\oneTimeForm'));
+	                die();
+	            }
+	        } else {
+	            // LOGIN OK + STATUS = off
+	            // TODO: Show same error of invalid credentials
+	            user_logout();
+	        }
+	    }
+	}
+
+	public function isStatusOn($responseData, $appid) {
+	    return $responseData->{"operations"}->{$appid}->{"status"} === "on";
+	}
+
+	public function isSecondFactorEnabled($responseData, $appid) {
+	    return property_exists($responseData->{"operations"}->{$appid}, "two_factor");
+	}
+
+	public function userProperlyLogged($account) {
+	    if (!empty($_POST['latch_otp'])) {
+	        DefaultController::checkSecondFactor($account);
+	    } else {
+	        $latch_account = DefaultController::getLatchId($account->id());
+	        if ($latch_account) {
+	            $statusResponse = DefaultController::getLatchStatus($latch_account);
+	            DefaultController::processStatusResponse($statusResponse, $account);
+	        }
+	    }
+	}
+
+	public function checkSecondFactor($account) {
+	    $storedToken = DefaultController::retrieveSecondFactor($account->id());
+	    DefaultController::removeSecondFactor($account->id());
+	    if ($_POST['latch_otp'] != $storedToken) {
+	    	// TODO: Show same error of invalid credentials
+	        user_logout();
+	    }
+	}
+
+/*
+ * Inserts the rendered second factor form into an HTML document.
+ */
+	public function buildHTML($htmlStructure) {
+	    global $base_url;
+		return '<html><head>'
+	            . '<style>'
+	            . '.twoFactorContainer { display:block; width:300px; margin: 5% auto 0 auto; text-align: center; border: solid 1px rgb(184, 184, 184); border-radius:5px}'
+	            . '.twoFactorHeader {float:left; background: #00b9be; color: #FFF; width:100%; border-top-left-radius: 5px; border-top-right-radius: 5px; font-family: sans-serif;}'
+	            . '.twoFactorHeader h3 {float: left; margin-left: 10px;}'
+	            . '.twoFactorHeader img {width: 45px; height: auto; float:left; margin-top: 5px; margin-left:20px}'
+	            . '.twoFactorForm {clear:left; padding-top:10px;}'
+	            . 'input {margin-top:10px}'
+	            . 'input[type="submit"] {width:65px;}'
+	            . '#edit-latch-otp {width:100px;}'
+	            . '</style>'
+	            . '</head><body>'
+	            . '<div class="twoFactorContainer">'
+	            . '<div class="twoFactorHeader"><img src="' . $base_url . '/modules/latch/symbol.png"><h3>One-Time Password</h3></div><div class="twoFactorForm">'
+	            . drupal_render($htmlStructure)
+	            . '</div></body></html>';
+	}
+
+	public function storeSecondFactor($otp, $uid) {
+	    db_update('latch')->fields(array('two_factor' => $otp))->condition('uid', $uid)->execute();
+	}
+
+	public function retrieveSecondFactor($uid) {
+	    $query = db_query("SELECT * FROM {latch} WHERE uid=:uid", array(':uid' => $uid));
+	    $userLatchData = $query->fetchObject();
+	    return ($userLatchData) ? $userLatchData->two_factor : NULL;
+	}
+
+	public function removeSecondFactor($uid) {
+	    db_update('latch')->fields(array('two_factor' => NULL))->condition('uid', $uid)->execute();
+	}
+}
---
+++ src/Form/pairForm.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\latch\Form\pairForm
+ */
+namespace Drupal\latch\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\latch\Controller\DefaultController;
+
+/**
+ * Configure latch settings for this site.
+ */
+class pairForm extends FormBase {
+  /** 
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'latch_pair_form';
+  }
+
+  /** 
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('latch.settings');
+    $uid = \Drupal::currentUser()->id();
+
+    if ($config->get('latch_appid') == '' | $config->get('latch_secret') == '') {
+      $form['insert_config'] = array(
+        '#markup' => '<p>' . t('Please insert your <a href=":latch-settings">Application ID and Secret</a> before to pair with Latch', [':latch-settings' => \Drupal::url('latch.settings')]) . '</p>',
+      );
+    } else {
+      if (DefaultController::getLatchId($uid)){
+          // Latch info
+          $form['latch_info'] = array(
+            '#markup' => '<p>User already paired</p>'
+          );
+          //Unpair
+          $form['latch_pair'] = array(
+              '#type' => 'submit',
+              '#value' => t('Unpair'),
+          );
+
+      } else {
+          // Latch info
+          $form['latch_info'] = array(
+            '#markup' => '<p>Generate and insert token to pair your account</p>'
+          );
+
+          // Page title field
+          $form['latch_token'] = array(
+            '#type' => 'textfield',
+            '#title' => $this->t('Latch Pairing Token'),
+            '#required' => TRUE,
+          );
+          //Pair
+          $form['latch_pair'] = array(
+              '#type' => 'submit',
+              '#value' => t('Pair Account'),
+          );
+      }
+    }
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}.
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+
+  }
+
+  /** 
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $token = $form_state->getValue('latch_token');
+    $uid = \Drupal::currentUser()->id();
+    $latch_account = DefaultController::getLatchId($uid);
+
+    if ($latch_account){
+      DefaultController::unpairAccount($latch_account);
+    } else {
+      DefaultController::pairAccount($token);
+    }
+  }
+}
